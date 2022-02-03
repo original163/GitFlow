69ea21a161f0a4f54e0bc8a84f4f80c840ab38ef
original163 <original163@mail.ru>
2022-02-03 16:01:19 +0400

    .xcworkspacedata added to .gitignore

---
+++ GitFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFlow
+//
+//  Created by Денис Денисов on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
